7fc701fa830db1de0177579922ece610ab17022f
Terrence Curran <tcurran@approachingpi.com>
2015-01-16 02:13:00 -0500

    changed to new JAX-RS 2.0 client format.

---
+++ clients/fitbit/src/main/java/com/wirelust/personalapi/client/fitbit/FitBitApiClient.java
@@ -0,0 +1,23 @@
+package com.wirelust.personalapi.client.fitbit;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+
+
+/**
+ * 1/16/15
+ *
+ * @Author T. Curran
+ */
+public interface FitBitApiClient {
+
+	@GET
+	@Path("/1/user/-/profile.json")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUserProfile();
+}
