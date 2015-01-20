6ec9d1dcdd7e2fa26c67db38b09ca2eab996eee3
Terrence Curran <tcurran@approachingpi.com>
2015-01-20 02:04:12 -0500

    fitbit endpoint for weight goal

--- clients/fitbit/src/main/java/com/wirelust/personalapi/client/fitbit/representations/ActivityGoalsType.java
+++ clients/fitbit/src/main/java/com/wirelust/personalapi/client/fitbit/representations/ActivityGoalsType.java
@@ -1,27 +1,16 @@
 package com.wirelust.personalapi.client.fitbit.representations;
 
-import com.fasterxml.jackson.annotation.JsonRootName;
-
 /**
  * 1/17/15
  *
  * @Author T. Curran
  */
-@JsonRootName("goals")
-public class GoalsType {
-	Double weight;
+public class ActivityGoalsType {
+
 	Integer caloriesOut;
 	Double distance;
 	Integer floors;
 	Integer steps;
-
-	public Double getWeight() {
-		return weight;
-	}
-
-	public void setWeight(Double weight) {
-		this.weight = weight;
-	}
 
 	public Integer getCaloriesOut() {
 		return caloriesOut;
