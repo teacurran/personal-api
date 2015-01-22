d646c50e3f93d235f5aad5ac1fc0533e2d75bd44
Terrence Curran <tcurran@approachingpi.com>
2015-01-21 22:07:14 -0500

    fitbit api endpoint for /1/user/-/foods/log/water/goal.json

---
+++ clients/fitbit/src/main/java/com/wirelust/personalapi/client/fitbit/representations/WaterGoalType.java
@@ -0,0 +1,19 @@
+package com.wirelust.personalapi.client.fitbit.representations;
+
+/**
+ * 1/17/15
+ *
+ * @Author T. Curran
+ */
+public class WaterGoalType {
+
+	Double goal;
+
+	public Double getGoal() {
+		return goal;
+	}
+
+	public void setGoal(Double goal) {
+		this.goal = goal;
+	}
+}
