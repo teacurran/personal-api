36c1c13323ee6fb14ae75944978bceeecb382e14
Terrence Curran <tcurran@approachingpi.com>
2015-03-21 18:33:10 -0400

    custom window config and plural naming strategy

---
+++ personal-api/src/main/java/com/approachingpi/jpa/PluralNamingStrategy.java
@@ -0,0 +1,34 @@
+package com.approachingpi.jpa;
+
+import org.hibernate.cfg.ImprovedNamingStrategy;
+
+import org.hibernate.cfg.ImprovedNamingStrategy;
+import org.jvnet.inflector.Noun;
+
+/**
+ * Date: May 5, 2010
+ *
+ * @author T. Curran
+ */
+public class PluralNamingStrategy extends ImprovedNamingStrategy {
+
+	private static final long serialVersionUID = 4674532118559124654L;
+
+	@Override
+	public String foreignKeyColumnName(String propertyName,
+			String propertyEntityName, String propertyTableName,
+			String referencedColumnName) {
+		return columnName(propertyName) + "_id";
+	}
+
+	@Override
+	public String classToTableName(String className) {
+		return toLowerCase(Noun.pluralOf(super.classToTableName(className)));
+	}
+
+	// IMPORTANT! To ensure compatibility across windows and unix, convert all table
+	// names to lowercase
+	private String toLowerCase(String name) {
+		return name.toLowerCase();
+	}
+}
