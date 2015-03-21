d91ae561d62afd3ce2ed5eac67d2b3c13314137e
Terrence Curran <tcurran@approachingpi.com>
2015-03-21 18:32:44 -0400

    custom window config and plural naming strategy

---
+++ personal-api/src/main/java/com/approachingpi/web/view/CustomWindowConfig.java
@@ -0,0 +1,25 @@
+package com.approachingpi.web.view;
+
+import javax.enterprise.inject.Specializes;
+import javax.faces.context.FacesContext;
+
+import org.apache.deltaspike.jsf.spi.scope.window.DefaultClientWindowConfig;
+
+/**
+ * Date: 29-01-2015
+ *
+ * @author T. Curran
+ *
+ * This class will remove the DeltaSpike URL Parameters that are added for the WindowScoped Views
+ * ?dsRid=191&windowId=-8996
+ *
+ * This will disable WindowScoped but we don't use it in the project so it is okay.
+ */
+@Specializes
+public class CustomWindowConfig extends DefaultClientWindowConfig {
+
+	@Override
+	public ClientWindowRenderMode getClientWindowRenderMode(FacesContext facesContext) {
+		return ClientWindowRenderMode.NONE;
+	}
+}
