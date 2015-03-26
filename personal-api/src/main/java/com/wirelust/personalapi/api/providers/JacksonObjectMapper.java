55ca60b30fceaf7b3db681ca58ec92629ab389ae
Terrence Curran <tcurran@approachingpi.com>
2015-03-26 17:34:34 -0400

    jackson configuration

---
+++ personal-api/src/main/java/com/wirelust/personalapi/api/providers/JacksonObjectMapper.java
@@ -0,0 +1,25 @@
+package com.wirelust.personalapi.api.providers;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Date: 26-03-2015
+ *
+ * @Author T. Curran
+ */
+public class JacksonObjectMapper extends ObjectMapper {
+
+	public static JacksonObjectMapper get() {
+
+		// Jackson 2.0
+		JacksonObjectMapper mapper = new JacksonObjectMapper();
+
+		mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, false);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+
+		return mapper;
+	}
+
+
+}
