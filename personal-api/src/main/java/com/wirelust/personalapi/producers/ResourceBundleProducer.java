a39949136c80d47bfcb445eca5323fc68372bd29
Terrence Curran <tcurran@approachingpi.com>
2015-03-27 02:12:05 -0400

    changed the I18n loading

--- personal-api/src/main/java/com/wirelust/personalapi/producers/ResourceBundleProducer.java
+++ personal-api/src/main/java/com/wirelust/personalapi/producers/ResourceBundleProducer.java
@@ -33,7 +33,7 @@ public class ResourceBundleProducer {
 	public ResourceBundle getLocalization() {
 		Locale locale = Locale.US;
 
-		return ResourceBundle.getBundle("com.wirelust.personalapi.locales.I18n", locale,
+		return ResourceBundle.getBundle("locales.I18n", locale,
 				new UTF8ResourceBundleControl());
 	}
 
