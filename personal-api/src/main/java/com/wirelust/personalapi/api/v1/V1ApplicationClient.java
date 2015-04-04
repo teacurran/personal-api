9364560cbd47e21c8b4d024cca56f083ceedab9b
Terrence Curran <tcurran@approachingpi.com>
2015-04-04 18:27:59 -0400

    api test client

---
+++ personal-api/src/main/java/com/wirelust/personalapi/api/v1/V1ApplicationClient.java
@@ -0,0 +1,56 @@
+package com.wirelust.personalapi.api.v1;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Date: 04-Apr-2015
+ *
+ * @author T. Curran
+ */
+public interface V1ApplicationClient {
+
+	@POST
+	@Path("/api/v1/register")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Response register(
+			@NotNull
+			@FormParam("accessCode")
+			final String inAccessCode,
+
+			@NotNull
+			@FormParam("client_id")
+			final String inClientId,
+
+			@NotNull
+			@Size(min = 3, max = 20)
+			@Pattern(regexp = "^[A-Za-z0-9_]+$")
+			@FormParam("username")
+			final String inUsername,
+
+			@NotNull
+			//@Email
+			@FormParam("email")
+			final String inEmail,
+
+			@NotNull
+			@Size(min = 5, max = 20)
+			@Pattern(regexp = "^[A-Za-z0-9_\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\|\\~\\`\\+\\=\\[\\]\\{\\}\\|\\\\]+$")
+			@FormParam("password")
+			final String inPassword,
+
+			@NotNull
+			@Size(min = 5)
+			@FormParam("fullName")
+			final String inFullName,
+
+			@FormParam("inviteCode")
+			final String inInviteCode);
+}
--- personal-api/src/test/java/test/com/wirelust/personalapi/ApiTest.java
+++ personal-api/src/test/java/test/com/wirelust/personalapi/ApiTest.java
@@ -1,14 +1,29 @@
 package test.com.wirelust.personalapi;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.wirelust.personalapi.api.v1.V1ApplicationClient;
+import com.wirelust.personalapi.client.fitbit.FitBitApiClient;
+import junit.framework.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
 import org.jboss.shrinkwrap.api.asset.FileAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -23,6 +38,13 @@ import org.slf4j.LoggerFactory;
 public class ApiTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ApiTest.class);
+
+	private static final String ROOT_URL = "http://127.0.0.1:8080";
+
+	ResteasyClient client;
+	ResteasyWebTarget target;
+	V1ApplicationClient v1ApplicationClient;
+
 
 	@Deployment
 	public static WebArchive create() {
@@ -57,9 +79,27 @@ public class ApiTest {
 		return testWar;
 	}
 
+	@Before
+	public void init() {
+		client = new ResteasyClientBuilder().build();
+		target = client.target(ROOT_URL);
+		v1ApplicationClient = target.proxy(V1ApplicationClient.class);
+	}
+
 	@Test
 	public void shouldBeAbleToCreateAccount() throws Exception {
 
+		Response response = v1ApplicationClient.register(
+			"xxxxxx",
+			"xxxxxx",
+			"tea",
+			"tea@grilledcheese.com",
+			"Terrence Curran",
+			"1234",
+			"1234"
+		);
+
+		Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatus());
 
 		// This will always fail because we are passing in an invalid authCode
 		//Assert.assertEquals(response.getStatus(), Status.OK.getStatusCode());
