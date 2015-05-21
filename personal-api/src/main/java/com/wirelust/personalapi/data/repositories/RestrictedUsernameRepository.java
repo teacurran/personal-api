5a8fdfdedd855ea9ae84e68545abfcfc2b924324
Terrence Curran <tcurran@approachingpi.com>
2015-05-21 15:21:15 -0400

    restricted username check unit test

---
+++ personal-api/src/main/java/com/wirelust/personalapi/data/repositories/RestrictedUsernameRepository.java
@@ -0,0 +1,21 @@
+package com.wirelust.personalapi.data.repositories;
+
+import com.wirelust.personalapi.data.model.RestrictedUsername;
+import org.apache.deltaspike.data.api.AbstractEntityRepository;
+import org.apache.deltaspike.data.api.Repository;
+
+/**
+ * Date: 21-May-2015
+ *
+ * @author T. Curran
+ */
+@Repository
+public abstract class RestrictedUsernameRepository extends AbstractEntityRepository<RestrictedUsername, Integer> {
+
+	public abstract RestrictedUsername findAnyByUsernameNormalized(final String usermane);
+
+	public boolean isRestricted(final String username) {
+		RestrictedUsername restrictedUsername = findAnyByUsernameNormalized(username);
+		return (restrictedUsername != null);
+	}
+}
