419ed20460ad0dcfa77a163bfc593a1c692e12d4
Terrence Curran <tcurran@approachingpi.com>
2016-01-10 18:27:01 -0500

    google fit representations

---
+++ personal-api/src/main/java/com/wirelust/personalapi/data/model/Activity.java
@@ -0,0 +1,11 @@
+package com.wirelust.personalapi.data.model;
+
+/**
+ * Date: 09-Jan-2016
+ *
+ * @author T. Curran
+ */
+public class Activity {
+
+
+}
--- personal-api/src/main/java/com/wirelust/personalapi/data/model/ActivityType.java
+++ personal-api/src/main/java/com/wirelust/personalapi/data/model/ActivityType.java
@@ -1,9 +1,44 @@
 package com.wirelust.personalapi.data.model;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Date: 09-Jan-2016
  *
  * @author T. Curran
  */
+@Entity
+@Access(AccessType.FIELD)
+@Cacheable
 public class ActivityType {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	@Column(length=200)
+	String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
