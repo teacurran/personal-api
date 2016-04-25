b81bb11e4b19107e0e9d8dfad6db98a135e24a1d
Terrence Curran <tcurran@approachingpi.com>
2016-04-24 20:58:03 -0400

    added jacoco and unit test for ResourceBundleProducer

---
+++ personal-api/src/test/java/test/com/wirelust/personalapi/producers/ResourceBundleProducerTest.java
@@ -0,0 +1,61 @@
+package test.com.wirelust.personalapi.producers;
+
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import javax.inject.Inject;
+
+import com.wirelust.personalapi.locales.UTF8ResourceBundleControl;
+import com.wirelust.personalapi.producers.ResourceBundleProducer;
+import com.wirelust.personalapi.qualifiers.ClasspathResource;
+import com.wirelust.personalapi.qualifiers.Localization;
+import org.jglue.cdiunit.AdditionalClasses;
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Date: 24-Apr-2016
+ *
+ * @author T. Curran
+ */
+@RunWith(CdiRunner.class)
+@AdditionalClasses(ResourceBundleProducer.class)
+public class ResourceBundleProducerTest {
+
+	@Inject
+	@ClasspathResource("default.properties")
+	Properties defaultProperties;
+
+	@Inject
+	@Localization
+	ResourceBundle localization;
+
+	@Inject
+	@ClasspathResource("invalid.properties")
+	Properties invalidProperties;
+
+	@Test
+	public void shouldBeAbleToLoadResource() {
+		Assert.assertEquals("default", defaultProperties.getProperty("applicationSetting"));
+	}
+
+	@Test
+	public void shouldBeAbleToReadLocalization() {
+		Assert.assertEquals("Sample Project", localization.getString("application.name"));
+	}
+
+	@Test
+	public void invalidPropertiesShouldNotLoad() {
+		Assert.assertNull(invalidProperties.getProperty("applicationSetting"));
+	}
+
+	@Test
+	public void loadLocaleWithReload() throws Exception {
+		UTF8ResourceBundleControl utf8ResourceBundleControl = new UTF8ResourceBundleControl();
+		localization = utf8ResourceBundleControl.newBundle("locales.I18n", Locale.US, "java.properties",
+			this.getClass().getClassLoader(), true);
+		Assert.assertEquals("Sample Project", localization.getString("application.name"));
+	}
+}
