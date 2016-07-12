7655e3eb17af36c5152594d6b9ae27aff4c48da2
Terrence Curran <tcurran@approachingpi.com>
2016-07-12 19:42:53 -0400

    unit test coverage

--- personal-api/src/test/java/test/com/wirelust/personalapi/api/v1/resources/AccountResourceTest.java
+++ personal-api/src/test/java/test/com/wirelust/personalapi/api/v1/resources/AccountResourceTest.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import com.wirelust.personalapi.api.exceptions.ApiException;
 import com.wirelust.personalapi.api.v1.representations.EnumErrorCode;
 import com.wirelust.personalapi.api.v1.resources.AccountResource;
+import com.wirelust.personalapi.data.model.Account;
 import com.wirelust.personalapi.data.model.ApiApplication;
 import com.wirelust.personalapi.data.repositories.AccountRepository;
 import com.wirelust.personalapi.data.repositories.ApiApplicationRepository;
@@ -79,6 +80,55 @@ public class AccountResourceTest {
 			Assert.fail();
 		} catch (ApiException e) {
 			assertEquals(EnumErrorCode.USERNAME_INVALID, e.getErrorCode());
+		}
+	}
+
+	@Test
+	public void shouldThrowExceptionWhenRegisterGetsExistingUsername() {
+
+		when(apiApplicationRepository.findBy(any(String.class))).thenReturn(new ApiApplication());
+		when(accountRepository.usernameIsValid(any(String.class))).thenReturn(true);
+		when(accountRepository.usernameExists(any(String.class))).thenReturn(true);
+
+		try {
+			accountResource.register("clientId", "username", null, null, null, null);
+			Assert.fail();
+		} catch (ApiException e) {
+			assertEquals(EnumErrorCode.USERNAME_EXISTS, e.getErrorCode());
+		}
+	}
+
+	@Test
+	public void shouldThrowExceptionWhenRegisterGetsRestrictedUsername() {
+
+		when(apiApplicationRepository.findBy(any(String.class))).thenReturn(new ApiApplication());
+		when(accountRepository.usernameIsValid(any(String.class))).thenReturn(true);
+		when(accountRepository.usernameExists(any(String.class))).thenReturn(false);
+		when(restrictedUsernameRepository.isRestricted(any(String.class))).thenReturn(true);
+
+		try {
+			accountResource.register("clientId", "username", null, null, null, null);
+			Assert.fail();
+		} catch (ApiException e) {
+			assertEquals(EnumErrorCode.USERNAME_EXISTS, e.getErrorCode());
+		}
+	}
+
+	@Test
+	public void shouldThrowExceptionWhenRegisterGetsDuplicateEmail() {
+
+		when(apiApplicationRepository.findBy(any(String.class))).thenReturn(new ApiApplication());
+		when(accountRepository.usernameIsValid(any(String.class))).thenReturn(true);
+		when(accountRepository.usernameExists(any(String.class))).thenReturn(false);
+		when(restrictedUsernameRepository.isRestricted(any(String.class))).thenReturn(false);
+		when(accountRepository.findAnyByEmail(any(String.class))).thenReturn(new Account());
+
+
+		try {
+			accountResource.register("clientId", "username", null, null, null, null);
+			Assert.fail();
+		} catch (ApiException e) {
+			assertEquals(EnumErrorCode.EMAIL_EXISTS, e.getErrorCode());
 		}
 	}
 
